030f3ad13a17024edcb321f41f0a90d4f919a82d
masyuks <myukimiftakhurrizqi21@gmail.com>
2020-10-08 13:29:50 +0700

    commit template framework codeigniter

---
+++ application/controllers/Index_controller.php
@@ -0,0 +1,19 @@
+
+<?php
+defined('BASEPATH') OR exit('No direct scipt access allowes');
+/**
+ * 
+ */
+class Index_controller extends CI_Controller
+{
+	public function __construct(){
+		parent::__construct();
+		$this->load->helper('url');
+		$this->load->helper('form');
+	}
+
+	public function index(){
+
+	}
+}
+?>
